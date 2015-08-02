078a4b70a1643fc85f0ff9a6683d6953ade706ed
Henrik Karlsson <hk.henrik@gmail.com>
2015-08-02 23:58:43 +0200

    Moved namespaces over to the C# conventions.

--- clip-mouse/App.xaml.cs
+++ clip-mouse/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace clip_mouse
+namespace ClipMouse
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- clip-mouse/MainWindow.xaml.cs
+++ clip-mouse/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace clip_mouse
+namespace ClipMouse
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
